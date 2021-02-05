3c348e154d29df41203f402846c0f45b93b366cc
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-05 13:54:54 +0800

    add class and show ingredients

--- Day16/oop-coffee-machine-start/main.py
+++ Day16/oop-coffee-machine-start/main.py
@@ -4,9 +4,23 @@ from money_machine import MoneyMachine
 
 menu = Menu()
 menu_items = menu.get_items()
-print(menu_items)
+#print(menu_items)
 
 latte = MenuItem('latte', '20', '30', '20', '70')
 espresso = MenuItem('espresso', '10', '30', '20', '40')
 cappuccino = MenuItem('cappuccino', '20', '20', '10', '50')
-print(espresso.ingredients)
+#print(espresso.ingredients)
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+while is_on:
+  option = menu.get_items()
+  choice = input(f"What do you want? ({option})?")
+  if choice == 'off':
+    is_on = False
+  elif choice == 'report':
+    coffee_report = coffee_maker.report()
+    money_report = money_machine.report()
+  else:
+    print(menu.find_drink(choice))
