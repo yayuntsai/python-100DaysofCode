a0721b4524089d57fd888a8feaf314ebaab0d3fe
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-07 15:54:11 +0800

    drow tomato

--- Day28/main.py
+++ Day28/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=100)
+
+canvas = Canvas(width=200, height=223)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+canvas.pack()
+
+window.mainloop()
