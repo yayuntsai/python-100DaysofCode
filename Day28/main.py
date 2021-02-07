c013f501e76bd06ea518e10f8d61dba280da42ba
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-07 22:06:10 +0800

    edit grid

--- Day28/main.py
+++ Day28/main.py
@@ -21,10 +21,19 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=100, bg=YELLOW)
 
+check_text = "✓"
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
-canvas.pack()
+timer_label = Label(window, text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40))
+timer_label.grid(row=0, column=1)
+start_button = Button(window, text="Start", activebackground=PINK)
+start_button.grid(row=2, column=0)
+reset_button = Button(window, text="Reset", activebackground=PINK)
+reset_button.grid(row=2, column=2)
+Label(window, text=check_text, font=(FONT_NAME, 40, "bold")).grid(row=3)
+
+canvas.grid(row=1, column=1)
 
 window.mainloop()
