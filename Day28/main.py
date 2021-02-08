8a0ae49a205b90f10215b0aad0f36106e8cf1f5e
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-08 11:14:37 +0800

    set time restart again after time's up

--- Day28/main.py
+++ Day28/main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -20,15 +20,17 @@ def reset_timer():
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
+    reps += 1
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
+    # START AND BREAK CONDITIONS
     # rep = 8th
     if reps % 8 == 0:
         count_down(long_break_sec)
     # rep = 2nd/4th/6th
-    elif reps % 2 ==0:
+    elif reps % 2 == 0:
         count_down(short_break_sec)
     # rep = 1st/3rd/5th/7th
     else:
@@ -36,20 +38,19 @@ def start_timer():
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
     #time format
     minute_text = math.floor(count/60)
-    sec_text = count%60
+    sec_text = count % 60
 
 
-    if sec_text<10:
+    if sec_text < 10:
         sec_text = f"0{sec_text}"
 
-
     canvas.itemconfig(timer_text, text=f"{minute_text}:{sec_text}")
-    if count>0:
+    if count > 0:
         window.after(1000, count_down, count-1)
-    print(count)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
