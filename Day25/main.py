2a9b33339e29137f388436ed625a45394fed50be
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-13 23:01:50 +0800

    pandas practice

--- Day25/main.py
+++ Day25/main.py
@@ -1,8 +1,15 @@
 import csv
+import pandas
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatues = []
-    for row in data:
-        if row[1] != 'temp':
-            temperatues.append(row[1])
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatues = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatues.append(row[1])
+
+
+data = pandas.read_csv("weather_data.csv")
+temp_list = data['temp']
+avg_temp_list = sum(temp_list) / len(temp_list)
+print("average temperature:", avg_temp_list)
