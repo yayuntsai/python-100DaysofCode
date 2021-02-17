12254b23a2c319e54927f4e234192a325d15ae03
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-17 21:31:14 +0800

    show the state name on the map

--- Day25/us-states-game-start/main.py
+++ Day25/us-states-game-start/main.py
@@ -23,13 +23,17 @@ print(all_states)
 
 # If answer state equals the state name in csv
 count_correct = 0
-for i in all_states:
-    if i == answer_state:
-        index_of_answer = all_states.index(i)
-        state_x = state_data.x[index_of_answer]
-        state_y = state_data.y[index_of_answer]
-        #state_y = state_data[state_data['state'] == answer_state].y
-        count_correct += 1 # count the correct number in total
-        print(state_x, state_y)
+
+if answer_state in all_states:
+    index_of_answer = all_states.index(answer_state)
+    state_x = state_data.x[index_of_answer]
+    state_y = state_data.y[index_of_answer]
+    count_correct += 1 # count the correct number in total
+
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    t.goto(state_x, state_y)
+    t.write(answer_state)
 
 turtle.mainloop()
