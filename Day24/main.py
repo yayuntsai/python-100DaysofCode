fcf3d3b14ad33a86a5297e089c66049b82ed6890
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-18 18:29:39 +0800

    day24 project

---
+++ Day24/main.py
@@ -0,0 +1,6 @@
+with open("Input/Names/invited_name.txt") as name_list:
+    names = name_list.read()
+
+    print(len(names))
+# with open("starting_letter.txt", mode="w") as file:
+#     file.replace(name, "")
