3eed511f33ef68b92f6789950f2aec2d2c8d266b
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-18 19:57:34 +0800

    get names and make letters

--- Day24/main.py
+++ Day24/main.py
@@ -1,6 +1,13 @@
-with open("Input/Names/invited_name.txt") as name_list:
-    names = name_list.read()
+with open("./Input/Names/invited_name.txt") as name_list:
+    names = name_list.readlines()
+    #names.strip()
+    print(names)
 
-    print(len(names))
-# with open("starting_letter.txt", mode="w") as file:
-#     file.replace(name, "")
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    placeholder = '[name]'
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(placeholder, stripped_name)
+        with open(f"./Output/ReadToSend/letter_of_{name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
