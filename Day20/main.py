aca721047018a21952064abd1f0df6fece3738a6
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-19 22:55:53 +0800

    create snake body

--- Day20/main.py
+++ Day20/main.py
@@ -4,6 +4,18 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
+turtle1 = Turtle()
+turtle2 = Turtle()
+turtle3 = Turtle()
+turtle1.color('white')
+turtle1.shape('square')
+turtle2.color('white')
+turtle2.shape('square')
+turtle2.goto((-20,0))
+turtle3.color('white')
+turtle3.shape('square')
+turtle3.goto((-40,0))
+
 
 
 
