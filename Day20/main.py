05fff85b6ac77b474f9ced6b2d312756345d5d8d
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-21 11:10:14 +0800

    make the snake body by using for loop

--- Day20/main.py
+++ Day20/main.py
@@ -4,17 +4,13 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
-turtle1 = Turtle()
-turtle2 = Turtle()
-turtle3 = Turtle()
-turtle1.color('white')
-turtle1.shape('square')
-turtle2.color('white')
-turtle2.shape('square')
-turtle2.goto((-20,0))
-turtle3.color('white')
-turtle3.shape('square')
-turtle3.goto((-40,0))
+
+starting_position = [(0,0), (-20,0), (-40,0)]
+for position in starting_position:
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    new_segment.goto(position)
+
 
 
 
