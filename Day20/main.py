eb558063b3b101fbd66adba68aaab07a5a18b50b
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-21 13:04:42 +0800

    turn the snake left

--- Day20/main.py
+++ Day20/main.py
@@ -23,6 +23,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg in segments:
-        seg.forward(20)
-
+    for seg_num in range(len(segments) - 1, 0, -1):  # (start, end , step)
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
