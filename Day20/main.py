bec490c57bcbaf47c012eff282033147a0d03aa1
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-22 16:26:52 +0800

    edit lines accroding to OOP

--- Day20/main.py
+++ Day20/main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,25 +9,13 @@ screen.bgcolor('black')
 screen.title('My Snake Game')
 screen.tracer(0) #Trace the screen and update screen after every events had done
 
-segments = []
-starting_position = [(0,0), (-20,0), (-40,0)]
+snake = Snake()
 
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup() # remove white line of trajectory
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-
+# Move snake
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
-    for seg_num in range(len(segments) - 1, 0, -1):  # (start, end , step)
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+screen.exitonclick()
