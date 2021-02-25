e2eb66ac8b07001774304a864ead364b87db1a99
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-25 10:43:34 +0800

    add conditional expressions of snake and food distance

--- Day20/main.py
+++ Day20/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 
@@ -10,6 +11,7 @@ screen.title('My Snake Game')
 screen.tracer(0) #Trace the screen and update screen after every events had done
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,7 +23,11 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.2)
     snake.move()
+
+    # Detect the distance between food and snake
+    if snake.head.distance(food) < 15:
+        print("collide")
 
 screen.exitonclick()
