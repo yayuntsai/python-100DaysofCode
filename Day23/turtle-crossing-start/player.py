7fd400a794f9740915315baf143bbcff88258257
anniesnoopymd <anniesnoopymd@gmail.com>
2021-03-04 23:40:11 +0800

    set the player movement

--- Day23/turtle-crossing-start/player.py
+++ Day23/turtle-crossing-start/player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
 class Player:
-    pass
+    turtle = Turtle()
+    turtle.setheading(90) # 設定turtle朝向哪一邊
+
+    turtle.shape("turtle")
+
+    def __init__(self):
+        pass
+
+    def up(self):
+        self.foward(0, -270)
+
