d8002c850d9aca38cb2a914bc74e2be4f03f153f
anniesnoopymd <anniesnoopymd@gmail.com>
2021-03-05 14:15:55 +0800

    create the car behavior

--- Day23/turtle-crossing-start/car_manager.py
+++ Day23/turtle-crossing-start/car_manager.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+        self.create_brick()
+
+    def create_brick(self):
+        new_car = Turtle('square')
+        new_car.penup()  # 不會畫軌跡
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.color(random.choice(COLORS))
+        rand_y = random.randint(-250, 250)  # 不讓食物落在y邊界上
+        new_car.goto(300, rand_y)
+        self.all_cars.append(new_car)
