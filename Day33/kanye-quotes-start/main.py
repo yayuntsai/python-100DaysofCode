24e262dd62b665f78afe10906a02dea3425e874f
anniesnoopymd <anniesnoopymd@gmail.com>
2021-03-07 10:29:45 +0800

    fetch API and change quotes

--- Day33/kanye-quotes-start/main.py
+++ Day33/kanye-quotes-start/main.py
@@ -1,14 +1,13 @@
 from tkinter import *
 import requests
 
-KAYNEQUOTE = "Kanye Quote Goes HERE"
-
-response = requests.get(url="https://api.kanye.rest")
-data = response.json()
-KAYNEQUOTE = data['quote']
 
 def get_quote():
-    pass
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data['quote']
+    canvas.itemconfig(quote_text, text=quote) # 賦值給字卡
 
 
 
@@ -19,7 +18,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text=KAYNEQUOTE, width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kayne quote", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
