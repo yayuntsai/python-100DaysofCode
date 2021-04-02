16133bad6c69784f0c85242629fcd11ebee4364a
anniesnoopymd <anniesnoopymd@gmail.com>
2021-04-02 18:24:13 +0800

    create a list saved the top 100 songs

--- Day46/main.py
+++ Day46/main.py
@@ -4,10 +4,22 @@ import requests
 # with open("website.html") as file:
 #     contents = file.read()
 
-url = "https://www.billboard.com/charts/hot-100/2021-04-01"
+enter_date = input("Which year do you want to travel to?(Format: YYYY-MM-DD)")
+
+url = f"https://www.billboard.com/charts/hot-100/{enter_date}"
 response = requests.get(url)
 html_doc = response.text
 
 
+
+
 soup = BeautifulSoup(html_doc, "html.parser")
-print(soup)
+articles = soup.find_all("span", "chart-element__information__song")
+
+article_texts = []
+
+for article in articles:
+    text = article.get_text()
+    article_texts.append(text)
+
+print(article_texts)
