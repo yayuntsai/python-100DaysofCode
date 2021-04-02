1856a0164260a48c26ff9c26fac60b253a323146
anniesnoopymd <anniesnoopymd@gmail.com>
2021-04-02 21:33:14 +0800

    prepare spotipy to connect to spotify

--- Day46/main.py
+++ Day46/main.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # with open("website.html") as file:
 #     contents = file.read()
+CLIENT_ID = "d904616d0451406b9267cc19c9c7747b"
+CLIENT_SECRET = "9e115fb01a554a87bf7162be3d859b6d"
+OAUTH_AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
 
 enter_date = input("Which year do you want to travel to?(Format: YYYY-MM-DD)")
 
 url = f"https://www.billboard.com/charts/hot-100/{enter_date}"
 response = requests.get(url)
 html_doc = response.text
-
-
 
 
 soup = BeautifulSoup(html_doc, "html.parser")
@@ -23,3 +27,14 @@ for article in articles:
     article_texts.append(text)
 
 print(article_texts)
+
+
+
+# Call Spotify API
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET))
+
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
