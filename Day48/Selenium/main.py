028da539ba7ee8840d1091e24ad1e41e4fa8f30a
anniesnoopymd <anniesnoopymd@gmail.com>
2021-04-13 18:30:12 +0800

    avoid inference

--- Day48/Selenium/main.py
+++ Day48/Selenium/main.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options
+options.add_argument("--disable-notifications") #不啟用通知
 
 # 開啟瀏覽器視窗(Chrome)
 # 方法一：執行前需開啟chromedriver.exe且與執行檔在同一個工作目錄
 chrome_driver_path = "./chromedriver"
-chrome_driver = webdriver.Chrome(executable_path=chrome_driver_path)
+chrome_driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=options)
 
 chrome_driver.get("http://www.python.org")
 search_bar = chrome_driver.find_element_by_name("q")
@@ -14,7 +18,9 @@ logo = chrome_driver.find_element_by_class_name("python-logo")
 
 # get element with Xpath
 search_input = chrome_driver.find_element_by_xpath('//*[@id="container"]/li[8]/ul/li[2]/a')
-# print(search_input.text)
+search_input.send_keys("selenium")
+search_btn = chrome_driver.find_element_by_xpath('//*[@id="submit"]')
+search_btn.click()
 
 
 
